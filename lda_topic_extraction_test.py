db7eece3973eeb4223de97eb37a2bb6b51a7f428
knie815 <katenie2000@gmail.com>
2024-10-29 10:04:43 -0400

    added ouput comment

--- lda_topic_extraction_test.py
+++ lda_topic_extraction_test.py
@@ -21,7 +21,17 @@ corpus = [dictionary.doc2bow(doc) for doc in tokenized_docs]
 lda_model = LdaModel(corpus, num_topics=2, id2word=dictionary)
 # Print the topics
 pprint(lda_model.print_topics())
+# Print output:
+# [(0,
+#   '0.038*"learning" + 0.037*"and" + 0.032*"such" + 0.032*"recognition" + '
+#   '0.030*"in" + 0.030*"various" + 0.030*"applications" + 0.029*"algorithms" + '
+#   '0.028*"used" + 0.028*"speech"'),
+#  (1,
+#   '0.051*"learning" + 0.043*"data" + 0.039*"machine" + 0.033*"and" + '
+#   '0.026*"statistical" + 0.026*"through" + 0.026*"analysis" + '
+#   '0.026*"extracting" + 0.026*"insights" + 0.025*"from"')]
 
 coherence_model = CoherenceModel(model=lda_model, texts=tokenized_docs, dictionary=dictionary, coherence='c_v')
 coherence_score = coherence_model.get_coherence()
 print(f"coherence_score: {coherence_score}")
+# coherence_score: 0.46366842544437337
